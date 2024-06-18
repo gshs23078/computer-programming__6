cf70e35b885b80120a7d2441ee6e4137d246bcc7
gshs23078 <129289574+gshs23078@users.noreply.github.com>
2024-06-18 21:49:07 +0900

    Update H.cpp

--- H.cpp
+++ H.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
-#include <vector>
-#define endl "\n"
-#define int long long
 using namespace std;
-vector<bool> Right;
-void f(int h, int n){
-	if(!h) return;
-	int k=1;
-	if(k<<h >n){
-		Right.push_back(0);
-		f(h-1, n);
+#define endl '\n'
+#define fori(i, a, b) for(int i=a; i<b; i++)
+#define ford(i, a, b) for(int i=a;i>=b; i--)
+
+int h, n;
+int ans;
+
+void f(int node, int layer, int left){
+	if(left<1<<layer) f(node*2, layer-1, left);
+	else if(left==1<<layer){
+		ans=node;
+		return;
 	}
-	else if(k<<h ==n) return;
-	else{
-		Right.push_back(1);
-		f(h-1, n-(k<<h));
-	}
+	else f(node*2+1, layer-1, left-(1<<layer));
 	return;
 }
-signed main(){
-	int h, n, ans=1;
+
+main(){
 	cin >> h >> n;
-	f(h, n);
-	int l=Right.size();
-	for(int i=0; i<l; i++)
-		ans=ans*2+Right[i];
+	f(1, h, n);
 	cout << ans;
 }
