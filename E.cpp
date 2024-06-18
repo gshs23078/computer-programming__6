4e0bbab76c8245ee103cd3396fdacb06c296a335
gshs23078 <129289574+gshs23078@users.noreply.github.com>
2024-06-18 22:12:03 +0900

    Update E.cpp

--- E.cpp
+++ E.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <string>
-#include <queue>
-#include <stack>
-#define int long long
-#define endl "\n"
-#define fori(i, a, b) for(int i=a; i<b; i++)
-#define ford(i, a, b) for(int i=a;i>=b; i--)
 using namespace std;
-string solve(string a, string b){
-	int l=a.length();
-	if(l==1) return a.substr(0, 1);
-	int k;
-	for(k=0; k<l; k++){
-		if(a[k]==b[0]) break;
-	}
-	if(k==0){
-		string p=a.substr(0, 1);
-		string q=solve(a.substr(k+1), b.substr(k+1));
-		q.append(p);
-		return q;
-	}
-	string p=solve(a.substr(0, k), b.substr(1, k));
-	if(k==l-1){
-		p.append(1, b[0]);
-		return p;
-	}
-	string q=solve(a.substr(k+1), b.substr(k+1));
-	p.append(q);
-	p.append(1, b[0]);
-	return p;
+
+string f(string in, string pre){
+	int l=in.length();
+	if(l<=1) return in;
+	
+	int i=-1;
+	while(in[++i]!=pre[0]);
+	
+	string nextin, ans, nextpre;
+	nextin.append(in, 0, i);
+	nextpre.append(pre, 1, i);
+	ans.append(f(nextin, nextpre));
+	
+	string innext, prenext;
+	innext.append(in, i+1, l-i);
+	prenext.append(pre, i+1, l-i);
+	ans.append(f(innext, prenext));
+	
+	ans.append(in, i, 1);
+	return ans;
 }
-signed main(){
-	string stra, strb;
-	cin >> stra >> strb;
-	cout << solve(stra, strb);
+
+main(){
+	string in, pre;
+	cin >> in >> pre;
+	cout << f(in, pre);
 }
